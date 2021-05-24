cda1352569b2f72b10024679e99258781a091e31
Gilbertoans <anschaugilberto@gmail.com>
2021-05-24 11:20:48 -0300

    criado comida da cobrinha

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ snake[0] = {
     y: 8 * box
 }
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 let direction = "right";
 
 function criarBG() {
@@ -19,6 +24,11 @@ function criarCobrinha(){
         context.fillStyle= "green"
         context.fillRect(snake[i].x, snake[i].y, box , box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red"
+    context.fillRect(food.x , food.y, box , box);
 }
 
 
@@ -39,6 +49,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
